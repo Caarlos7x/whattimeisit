7ad6d8476f88ef0ba443cba299abeaf62013180b
Carlos Augusto <95699480+Caarlos7x@users.noreply.github.com>
2022-07-11 21:21:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function carregar() {
+  var intervald = window.setInterval(() => 
+  {carregar() },1000);  
+  var msg = window.document.getElementById('msg')
+  var img = window.document.getElementById('morning')
+  var data = new Date()
+  var hora = data.getHours()
+  var minuto = data.getMinutes()
+  var segundo = data.getSeconds()
+  var col = document.getElementById('messa')
+  var span = document.getElementById('messa')
+  
+  var hora = 
+      
+  msg.innerHTML = `Agora são ${hora}:${minuto}:${segundo}`
+
+  if (hora >= 0 && hora < 12) {
+    // BOM DIA
+    img.src = 'morn.jpg'
+    document.body.style.background = '#FAF1D6'
+    span.textContent = 'Bom dia!'
+  } else if (hora >= 12 && hora <= 18) {
+    // BOA TARDE
+    img.src = 'aftern.jpg'
+    document.body.style.background = '#FDAFAB'
+    span.textContent = 'Boa tarde!'
+  } else {
+    // BOA NOITE
+    img.src = 'night.jpg'
+    document.body.style.background = 'black'
+    document.body.style.color = 'grey'
+    col.style.color = 'grey'
+    span.textContent = 'Boa noite!'
+  }
+
+}
